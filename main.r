e7fe88c77dd1a8e6afda797354e966c5042db180
unknown <fancellilorenzo1@gmail.com>
2022-05-05 12:44:12 +0200

    main 1.1

--- main.r
+++ main.r
@@ -1,6 +1,22 @@
 source("gbm.r")
 source("stock.r")
 
-paths<-GBMf(s0, 250, rets, sigmas, cdcorr)
+paths<-data.frame(t(GBMf(s0, 250, rets, sigmas, cdcorr)))
 
-plot(as.numeric(paths[1,]))
+data1 <- data.frame(c(2:nrow(paths)-1))
+for(i in colnames(paths)[1:length(colnames(paths))]){
+  v = as.vector(unlist(paths[i]))
+  vr = diff(v)/(v[-length(v)])
+  data1[i] <- vr
+}
+
+data1 <- data1[-1]
+
+for( i  in seq(from =1, to = nrow(data1), by = 1)){
+  
+  pr <- data1[i,]/length(data[i,])
+  data1[i,] <- pr
+  
+}
+
+
