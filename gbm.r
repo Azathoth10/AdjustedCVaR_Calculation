9d7d727bd99a24089906d39f8d2c91da03c32aa8
unknown <fancellilorenzo1@gmail.com>
2022-05-05 10:18:30 +0200

    GBM function 1.1

--- gbm.r
+++ gbm.r
@@ -1,7 +1,32 @@
-GBMf <- function(s0, t, m, sig, I, ch){
-  normvars <- rnorm(nrow(ch))
-  corrWiener <- ch%*% normvars
-  drift <- (m - (sig^2)/2)*t
-  rand <- sig*sqrt(t)*corrWiener
-  print(drift + rand)
-}
+#######GBM function 
+#Out###n number of paths for number of assets
+#In####
+#s0####Initial stock prices
+#t#####Time steps number
+#m#####mean
+#sig###standard deviations
+#ch####Lower triangular cholesky times correlation matrix
+#dt####time intervals
+
+GBMf <- function(s0, t, m, sig, ch, dt = 1./365){
+  
+  path <- cbind(s0, matrix(0, nrow(ch), t))
+  
+  normvars <- matrix(rnorm(nrow(ch), nrow(ch), t))
+  
+  
+  for(i in seq(from = 1, to = t, by = 1)){
+    cw <- ch %*% normvars[,i]
+    
+    rand <- sig*sqrt(dt)*cw
+    drift <- (m - (sig^2)/2)*dt
+    
+    st <- path[i]*exp(drift + rand) 
+    path[,i+1] <- st
+    
+  }
+  
+  path
+    
+  }
+  
